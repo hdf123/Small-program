2b594d60be6d38a38033ea89490a75123400695a
thbts <1205552014@qq.com>
2019-03-28 17:29:16 +0800

    Initial Commit

---
+++ pages/buy/buy.js
@@ -0,0 +1,73 @@
+// pages/buy/buy.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    _title: "购卡信息",
+    xlist:[
+      { types: '购买项目', information:'返金卡'},
+      { types: '购买时间', information:'2019.03.22'},
+      { types: '购买金额', information:'￥300'},
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: this.data._title
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,84 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    kname:"Jim potJim",
+    mlist:[
+      { url: '', txt: "咨询信息"},
+      { url: '../instructions/instructions', txt: "购买须知"},
+      { url: '', txt: "关于我们"}
+    ]
+  },
+  back: function (event) {
+    wx.navigateTo({
+      title: "goback",
+      url: event.currentTarget.dataset.url,
+    })
+  },
+  bik:function(){
+    this.setData({
+      kname:"改名"
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options){
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
