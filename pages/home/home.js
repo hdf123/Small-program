141d51807450ffc657bedc5c1eecacdc7d8e0993
thbts <1205552014@qq.com>
2019-04-08 15:33:27 +0800

    增加首页
    增加首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,22 +4,64 @@ Page({
   /**
    * 页面的初始数据
    */
+
   data: {
-
+    img:"../../image/choose_s.png",
+    shiftk:true,
+    msgList: [
+      { url: "url", title: "多地首套房贷利率上浮 热点城市渐迎零折扣时代" },
+      { url: "url", title: "李先生已通过返金卡成功找到优惠 1分钟前" },
+      { url: "url", title: "你想和一群有志青年一起过生日嘛？" }
+    ],
+    listData: [
+      { "code": "赵**", "text": "138****3399", "type": "39661元" },
+      { "code": "赵**", "text": "138****3399", "type": "39662元" },
+      { "code": "赵**", "text": "138****3399", "type": "39663元" },
+      { "code": "李**", "text": "138****3399", "type": "39664元" },
+      { "code": "李**", "text": "138****3399", "type": "39665元" },
+      { "code": "李**", "text": "138****3399", "type": "39666元" }
+    ]
   },
-
+  formSubmit: function (e) {
+    if (!this.data.shiftk){
+      this.setData({
+        shiftk: !this.data.shiftk,
+      })
+      this.termsp();
+    }
+    console.log('个人信息：', e.detail.value);
+  },
+  terms:function(){
+    this.setData({
+      shiftk: !this.data.shiftk,
+    })
+    this.termsp();
+  },
+  ttt:function(){
+    console.log(111);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.termsp();
   },
-
+  termsp:function(){
+    if (this.data.shiftk) {
+      this.setData({
+        img: "../../image/choose_s.png"
+      })
+    } else {
+      this.setData({
+        img: "../../image/choose.png"
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
